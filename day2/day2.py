47505e3d712f7e511070a3cf5679899daf700169
Gemma McLean <gemixin@gmail.com>
2022-12-02 12:26:05 +0000

    Replaced for loops with list comps for day 2

--- day2/day2.py
+++ day2/day2.py
@@ -21,27 +21,14 @@ with open('day2/input.txt') as file_object:
 point_values = {'X': 1, 'Y': 2, 'Z': 3}
 # Dictionary of result combinations (format = [win, draw, loss])
 result_combos = {'A': ['Y', 'X', 'Z'], 'B': ['Z', 'Y', 'X'], 'C': ['X', 'Z', 'Y']}
+# Point values for win, draw, loss
+result_points = [6, 3, 0]
 
 
 def calculate_scores(rounds):
-    ''' Calculate the round scores'''
-    round_scores = []
-    for opponent, me in rounds:
-        round_score = 0
-        # Get the result as either 0, 1 or 2
-        result = result_combos[opponent].index(me)
-        # Win
-        if result == 0:
-            round_score += 6
-        # Draw
-        elif result == 1:
-            round_score += 3
-        # Loss -> no action as score would be 0
-        # Add point value of the shape you chose
-        round_score += point_values[me]
-        # Add the round score to the list
-        round_scores.append(round_score)
-    return round_scores
+    '''Calculate the score for each round'''
+    return [result_points[result_combos[opponent].index(me)] + point_values[me]
+            for opponent, me in rounds]
 
 
 '''Part One'''
@@ -52,18 +39,11 @@ print(sum(round_scores))
 '''Part Two'''
 # Create a new list using the same format as part one, based on whether or not a win,
 # loss or draw is needed
-new_rounds = []
-for opponent, me in rounds:
-    # If a loss is needed, select the choice at index 2
-    if me == 'X':
-        new_me = result_combos[opponent][2]
-    # If a draw is needed, select the choice at index 1
-    elif me == 'Y':
-        new_me = result_combos[opponent][1]
-    # If a win is needed, select the choice at index 0
-    elif me == 'Z':
-        new_me = result_combos[opponent][0]
-    new_rounds.append([opponent, new_me])
+
+# X = Loss, Y = Draw, Z = Win
+required_index = {'X': 2, 'Y': 1, 'Z': 0}
+new_rounds = [[opponent, result_combos[opponent][required_index[me]]]
+              for opponent, me in rounds]
 
 # Answer is the sum of the new round scores
 new_round_scores = calculate_scores(new_rounds)
